7086c0943a55b01fb9b74d4553c3fe4346592eee
Colin Smith <colinxsmith@gmail.com>
2025-01-31 20:53:28 +0000

    .

--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 from flask import Flask, request 
-from os import system
+
+
 import subprocess
 
 app = Flask(__name__)
+@app.after_request
+def add_headers(response):
+    response.headers.add('Content-Type', 'application/json')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'content-type, traceid, withcredentials')
+    response.status=200
+    return response
 
+@app.route("/")
+def helloWorld():
+      back={}
+      back['status'] = subprocess.getoutput('mpc')
+      back['playlist'] = subprocess.getoutput('mpc playlist | cat -n').split('\n')
+      return [back]
 @app.route('/dave', methods=[ 'GET'])
 def index():
    if request.method == 'GET':
       value = request.args.get('value')
+      seek = request.args.get('seek')
       back={}
       back['value'] = value
       if value!='':
          subprocess.getoutput('mpc play %s' % value)
+      try:
+         if seek.find('%')>-1:
+            subprocess.getoutput('mpc seek %s'%seek)
+         else:
+            subprocess.getoutput('mpc seek %s'%seek+'%')
+      except:seek='0%';subprocess.getoutput('mpc seek 0%')
       back['status'] = subprocess.getoutput('mpc')
       back['playlist'] = subprocess.getoutput('mpc playlist | cat -n').split('\n')
       return [back]
