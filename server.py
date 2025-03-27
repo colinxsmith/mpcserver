9fba8adbc812d17dc258c7faabc6e84ec183534f
Colin Smith <colinxsmith@gmail.com>
2025-03-27 18:05:06 +0000

    .

--- server.py
+++ server.py
@@ -66,12 +66,16 @@ def index():
         back['mp3files'] = subprocess.getoutput('ls /home/pi/sound/*.mp3').split('\n')
         if insert != None:
             insert = insert.replace('%26', '&')
+            insert = insert.replace('%58', ':')
+            insert = insert.replace('%59', ';')
             insert = insert.replace('%21', '!')
             insert = insert.replace('%23', '#')
             back['inserted'] = subprocess.getoutput('mpc insert "%s" ' % insert)
 
         if insert_station != None:
             insert_station = insert_station.replace('%26', '&')
+            insert_station = insert_station.replace('%58', ':')
+            insert_station = insert_station.replace('%59', ';')
             insert_station = insert_station.replace('%21', '!')
             insert_station = insert_station.replace('%23', '#')
             back['inserted_station'] = subprocess.getoutput(
